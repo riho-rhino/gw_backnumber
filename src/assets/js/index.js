fca7c3aef498adc7f424170ad97dcfafdf5b65a4
Riho yamamoto <r.yamamoto@rhino-inc.jp>
2025-04-18 16:22:58 +0900

    4.18

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,35 @@
-import hellow from "./_modules/_test"
+import $ from "jquery"
 
-hellow()
+
+opning();
+function tick(time) {
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            resolve()
+        }, time)
+    })
+}
+async function opning() {
+  await tick(300);
+  $('.text-selector').addClass('move');
+
+}
+
+
+
+
+
+const swiper = new Swiper(".swiper", {
+    loop: true,  
+    slidesPerView: 1, 
+    spaceBetween: 40, 
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction", 
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
